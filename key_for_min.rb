e2bb2f15166e9baa9b92c542881b9542d2020e3b
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-01 14:47:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,7 +14,7 @@ def key_for_min_value(hash)
   end 
 end
 
-# def key_for_min_value(hash)
+def key_for_min_value(hash)
 #   lowest_key = nil 
 #   lowest_value = Float::INFINITY 
-#   hash.each do |k, v| if v < lowest_value lowest_value = v lowest_key = k end end lowest_key end
+#   hash.each do |k, v| if v < lowest_value lowest_value = v lowest_key = k end end lowest_key end# 
