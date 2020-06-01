b8d7af2b5ad9baf13c073693414f914d2508218f
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-01 14:33:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return [] if name_hash.empty?
+  
+  the_name = name_hash.collect {|key, value| value.
 end
